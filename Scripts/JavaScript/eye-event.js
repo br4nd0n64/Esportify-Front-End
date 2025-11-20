51758c724d8ce76ae730c663322514656c0d73f2
br4nd0n64 <brandon_noirot@outlook.com>
2025-11-20 11:48:14 +0100

    JavaScript Événement Bouton Montrer-Cacher Mot De Passe

---
+++ Scripts/JavaScript/eye-event.js
@@ -0,0 +1,16 @@
+﻿const passwordInputElement = document.querySelector(".password-field input");
+const eyeImgElement = document.querySelector(".password-field img:nth-of-type(2)");
+
+const closeEyeIcon = "../Assets/Close_Eye.png";
+const openEyeIcon = "../Assets/Open_Eye.png";
+
+eyeImgElement.addEventListener("click", () => {
+  if(document.getElementById("eye").getAttribute("src").match(openEyeIcon)){
+    document.getElementById("eye").setAttribute("src", closeEyeIcon);
+    passwordInputElement.setAttribute("type", "password");
+  }
+  else{
+    document.getElementById("eye").setAttribute("src", openEyeIcon);
+    passwordInputElement.setAttribute("type", "reveal password");
+  }
+});
